cfbffe4e6eb9f7c7fda862a131b17169be953cc8
nguyenanhung <info@nguyenanhung.com>
2022-07-15 02:06:27 +0700

    Update

---
+++ src/Crypt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project htpasswd
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 15/07/2022
+ * Time: 02:04
+ */
+
+namespace nguyenanhung\Libraries\Htpasswd;
+
+
+class Crypt extends \axy\htpasswd\Crypt
+{
+
+}
